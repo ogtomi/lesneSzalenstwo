2733d93079c151de6eef69210fa006d72d7a9d4c
Krzysztof Jeschke <jeschke.krzysztof44@gmail.com>
2021-08-26 12:30:21 +0200

    Info Screen v3

--- src/screens/InfoScreen.tsx
+++ src/screens/InfoScreen.tsx
@@ -1,10 +1,13 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
+import { Dimensions } from "react-native";
 import {
+  Button,
   FlatList,
   ImageBackground,
   StyleSheet,
   Text,
+  TouchableOpacity,
   View,
 } from "react-native";
 import TittleTattle from "../InfoScreen/TittleTattle";
@@ -12,19 +15,31 @@ import TittleTattle from "../InfoScreen/TittleTattle";
 const DATA = [
   {
     id: "1",
-    title: "Ciekawosta 1",
+    title: "Ciekawostka 1",
     content:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua",
   },
   {
     id: "2",
-    title: "Ciekawosta 1",
+    title: "Ciekawostka 1",
     content:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua",
   },
   {
     id: "3",
-    title: "Ciekawosta 1",
+    title: "Ciekawostka 1",
+    content:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua",
+  },
+  {
+    id: "4",
+    title: "Ciekawostka 1",
+    content:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua",
+  },
+  {
+    id: "5",
+    title: "Ciekawostka 1",
     content:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua",
   },
@@ -32,48 +47,44 @@ const DATA = [
 
 const backgroundURL = require("../../img/tittletattle.jpg");
 
-export default function InfoScreen() {
+export default function InfoScreen({ history }) {
   return (
     <ImageBackground source={backgroundURL} style={styles.imgBackground}>
       {/* <View style={styles.container}>
         <Text style={styles.title}>Czy wiesz, że?</Text> */}
-      <FlatList style={styles.container}>
-        <TittleTattle
-          title={"Ciekawostka 1"}
-          content={
-            "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla a placerat leo. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Sed nec interdum mi, nec fermentum nisl. Quisque euismod sagittis felis, et semper neque dignissim sit amet. Curabitur eleifend sit amet nisl eget accumsan. Sed porta pretium efficitur. Praesent ac urna in urna tincidunt ultrices. Integer et"
-          }
-        />
-        <TittleTattle
-          title={"Ciekawostka 2"}
-          content={
-            "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla a placerat leo. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Sed nec interdum mi, nec fermentum nisl. Quisque euismod sagittis felis, et semper neque dignissim sit amet. Curabitur eleifend sit amet nisl eget accumsan. Sed porta pretium efficitur. Praesent ac urna in urna tincidunt ultrices. Integer et"
-          }
-        />
-        <TittleTattle
-          title={"Ciekawostka 3"}
-          content={
-            "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla a placerat leo. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Sed nec interdum mi, nec fermentum nisl. Quisque euismod sagittis felis, et semper neque dignissim sit amet. Curabitur eleifend sit amet nisl eget accumsan. Sed porta pretium efficitur. Praesent ac urna in urna tincidunt ultrices. Integer et"
-          }
-        />
-      </FlatList>
+      <FlatList
+        ListHeaderComponent={<Text style={styles.title}>Czy wiesz, że?</Text>}
+        style={styles.container}
+        data={DATA}
+        renderItem={({ item }) => (
+          <TittleTattle title={item.title} content={item.content} />
+        )}
+        keyExtractor={(item) => item.id}
+      ></FlatList>
       {/* <Text style={styles.credits}>
           Autorzy: Krzysztof Jeschke i Tomasz Lewandowski
         </Text>
       </View> */}
+      <TouchableOpacity
+        style={styles.buttonView}
+        onPress={() => history.goBack()}
+      >
+        <Text style={styles.buttonViewText}>{"<<<"}</Text>
+      </TouchableOpacity>
     </ImageBackground>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    //flex: 1,
     //backgroundColor: "#fff",
-    alignItems: "center",
+    //alignItems: "center",
     //justifyContent: "center",
   },
   title: {
-    marginTop: 40,
+    alignSelf: "center",
+    //    marginTop: 40,
     //backgroundColor: "#fff",
     fontSize: 30,
     //alignItems: "center",
@@ -88,7 +99,23 @@ const styles = StyleSheet.create({
   },
   imgBackground: {
     //width: "100%",
+    marginTop: 40,
     height: "100%",
     flex: 1,
+  },
+  buttonView: {
+    marginBottom: 10,
+    //width: Dimensions.get("window").width,
+    marginLeft: 5,
+    marginRight: 5,
+    height: 60,
+    backgroundColor: "rgb(0, 143, 52)",
+    alignContent: "center",
+    justifyContent: "center",
+    borderRadius: 10,
+  },
+  buttonViewText: {
+    fontSize: 45,
+    alignSelf: "center",
   },
 });
