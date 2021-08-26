b63457e2903355854663181cb31372fd2b0ebcd0
Tomek Lewandowski <tomeklewandowski570@gmail.com>
2021-08-26 10:50:58 +0200

    main screen

--- src/screens/MainScreen.tsx
+++ src/screens/MainScreen.tsx
@@ -1,31 +1,77 @@
-import React from 'react'
-import { TouchableOpacity, View, Text } from 'react-native'
+import React from "react";
+import { TouchableOpacity, View, Text, StyleSheet } from "react-native";
 
 const MainScreen = ({ history }) => {
-    return (
-        <View>
-            <TouchableOpacity onPress={() => history.push("InfoScreen")}>
-                <Text>
-                    InfoScreen
-                </Text>
-            </TouchableOpacity>
-            <TouchableOpacity onPress={() => history.push("MapScreen")}>
-                <Text>
-                    MapScreen
-                </Text>
-            </TouchableOpacity>
-            <TouchableOpacity onPress={() => history.push("PathsScreen")}>
-                <Text>
-                    PathsScreen
-                </Text>
-            </TouchableOpacity>
-            <TouchableOpacity onPress={() => history.push("ScanQrScreen")}>
-                <Text>
-                    ScanQrScreen
-                </Text>
-            </TouchableOpacity>
+  return (
+    <View style={styles.container}>
+      <View style={styles.columns}>
+        <View style={styles.rows}>
+          <TouchableOpacity
+            onPress={() => history.push("InfoScreen")}
+            style={styles.tiles}
+          >
+            <Text>InfoScreen</Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            onPress={() => history.push("MapScreen")}
+            style={styles.tiles}
+          >
+            <Text>MapScreen</Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            onPress={() => history.push("PathsScreen")}
+            style={styles.tiles}
+          >
+            <Text>PathsScreen</Text>
+          </TouchableOpacity>
         </View>
-    )
-}
+        <View style={styles.rows}>
+          <TouchableOpacity
+            onPress={() => history.push("InfoScreen")}
+            style={styles.tiles}
+          >
+            <Text>InfoScreen</Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            onPress={() => history.push("MapScreen")}
+            style={styles.tiles}
+          >
+            <Text>MapScreen</Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            onPress={() => history.push("PathsScreen")}
+            style={styles.tiles}
+          >
+            <Text>PathsScreen</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+    </View>
+  );
+};
 
-export default MainScreen
+export default MainScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    marginTop: 40
+  },
+  tiles: {
+    backgroundColor: "#228B22",
+    height: 100,
+    width: 100,
+    justifyContent: "center",
+    marginLeft: 7,
+    marginRight: 7,
+    marginTop: 7,
+    marginBottom: 7,
+  },
+  rows: {
+    flexDirection: "row",
+  },
+  columns: {
+    flexDirection: "column",
+  },
+});
