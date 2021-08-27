bb4a5498cb9948f53db944b116bb61645a969b27
Krzysztof Jeschke <jeschke.krzysztof44@gmail.com>
2021-08-27 10:12:26 +0200

    asdsa

--- src/screens/ConsultScreen.tsx
+++ src/screens/ConsultScreen.tsx
@@ -73,7 +73,7 @@ export default function ConsultScreen({ history }) {
       ></FlatList>
       <TouchableOpacity
         style={styles.buttonView}
-        onPress={() => history.push("/")}
+        onPress={() => history.goBack()}
       >
         <Text style={styles.buttonViewText}>{"<<<"}</Text>
       </TouchableOpacity>
